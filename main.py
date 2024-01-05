7f60037397c151defb54fca10df4083ef3404b2f
seka <121715900+lutherantz@users.noreply.github.com>
2024-01-05 14:07:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import time
 import fade
 import random
-import urllib3
 import threading
 from pytermx import *
 from requests import post
-from urllib.parse import urlencode
-
-urllib3.disable_warnings()
+from urllib.parse import urlencode 
 
 def Print(text: str):
     print(
@@ -18,13 +16,13 @@ def Input(text: str):
         f"\r            {Color.BRIGHT_BLUE}->{Color.BRIGHT_PURPLE} {text} {Color.BRIGHT_BLUE}-> {Color.RESET}", end="")
     return input()
 
-def get_al(params: str, payload: str = None, sectoken: str = ""):
-    """
-        'Difficulties are things that show a person what they are.' - Epictetus
-    """
+def get_ts():
+    return time.time()
 
 def send(item_id: int, device_id: int, install_id: int, cdid, openudid):
     for _ in range(2):
+        actual_ts = get_ts()
+
         req_apis = [
             "api15-core-useast1a.tiktokv.com",
             "api16-core-useast1a.tiktokv.com",
@@ -34,55 +32,47 @@ def send(item_id: int, device_id: int, install_id: int, cdid, openudid):
             "api31-core-useast1a.tiktokv.com",
         ]
 
-        version = random.choice(
-                    [247, 312, 322, 357, 358, 415, 422, 444, 466]
-        )
-
-        device = random.choice(
-                    ["SM-G9900", "sm-g950f", "SM-A136U1", "SM-M225FV", "SM-E426B", "SM-M526BR", "SM-M326B", "SM-A528B", "SM-F711B", "SM-F926B", "SM-A037G", "SM-A225F", "SM-M325FV", "SM-A226B", "SM-M426B", "SM-A525F"]
-        )
-
         params = urlencode({
             "iid"                  : f"{install_id}",
             "device_id"            : f"{device_id}",
-            "ac"                   : "5g",
+            "ac"                   : "wifi",
             "channel"              : "googleplay",
             "aid"                  : "1233",
             "app_name"             : "musical_ly",
-            "version_code"         : "250304",
-            "version_name"         : "25.3.4",
+            "version_code"         : "320503",
+            "version_name"         : "32.5.3",
             "device_platform"      : "android",
             "os"                   : "android",
-            "ab_version"           : "25.3.4",
+            "ab_version"           : "32.5.3",
             "ssmix"                : "a",
             "device_type"          : "SM-A405FN",
             "device_brand"         : "samsung",
-            "language"             : "zh-Hant",
-            "os_api"               : 28,
+            "language"             : "es-ES",
+            "os_api"               : 720,
             "os_version"           : 9,
             "openudid"             : openudid,
-            "manifest_version_code": 2022503040,
+            "manifest_version_code": 2023205030,
             "resolution"           : "1080*2110",
             "dpi"                  : 240,
-            "update_version_code"  : 2022503040,
-            "_rticket"             : 1704110807522,
+            "update_version_code"  : 2023205030,
+            "_rticket"             : actual_ts,
             "is_pad"               : 0,
             "app_type"             : "normal",
-            "sys_region"           : "TW",
+            "sys_region"           : "ES",
             "mcc_mnc"              : 45402,
-            "timezone_name"        : "Asia/Taipei",
+            "timezone_name"        : "Europe/Madrid",
             "carrier_region_v2"    : 454,
-            "app_language"         : "zh-Hant",
-            "carrier_region"       : "HK",
-            "ac2"                  : "wifi",
+            "app_language"         : "es-ES",
+            "carrier_region"       : "ES",
+            "ac2"                  : "wifi5g",
             "uoo"                  : 0,
-            "op_region"            : "HK",
+            "op_region"            : "ES",
             "timezone_offset"      : 28800,
-            "build_number"         : "25.3.4",
+            "build_number"         : "32.5.3",
             "host_abi"             : "arm64-v8a",
-            "locale"               : "zh-Hant-TW",
-            "region"               : "TW",
-            "ts"                   : 1704110807,
+            "locale"               : "es-ES",
+            "region"               : "ES",
+            "ts"                   : actual_ts,
             "cdid"                 : cdid
         })
 
@@ -94,11 +84,11 @@ def send(item_id: int, device_id: int, install_id: int, cdid, openudid):
             "item_id"                 : f"{item_id}",
             "is_ad"                   : 0,
             "follow_status"           : 0,
-            "session_id"              : 1704110807,
+            "session_id"              : actual_ts,
             "sync_origin"             : False,
             "follower_status"         : 0,
             "impr_order"              : 0,
-            "action_time"             : 1704110807,
+            "action_time"             : actual_ts,
             "tab_type"                : 0,
             "item_distribute_source"  : "for_you_page",
             "pre_hot_sentence"        : "",
@@ -108,9 +98,10 @@ def send(item_id: int, device_id: int, install_id: int, cdid, openudid):
             "pre_item_id"             : ""
         })
 
-        xargus, xladon = get_al(
-            params  = params,
-            payload = payload,
+        alg = get_al(
+            params=params,
+            payload=payload,
+            sec_device_id=device_id
         )
 
         tiktok_res = post(
@@ -122,11 +113,9 @@ def send(item_id: int, device_id: int, install_id: int, cdid, openudid):
             ),
             data    = payload,
             headers = {
-                "User-Agent"  : f"com.ss.android.ugc.trill/{version} (Linux; U; Android 11; fr_FR; {device}; Build/RP1A.200720.012; Cronet/58.0.2991.0)",
-                "X-Argus"     : xargus,
-                "X-Ladon"     : xladon,
+                "User-Agent"  : f"com.ss.android.ugc.trill/320503 (Linux; U; Android 11; fr_FR; SM-A405FN; Build/RP1A.200720.012; Cronet/58.0.2991.0)",
                 "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            }
+            } | alg,
         )
 
         if tiktok_res.text == "":
